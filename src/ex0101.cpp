5fc825f5e43c80d2d8bd68958d64a058d0ed245e
kaileebarnes_17 <kaileebarnes@mail.weber.edu>
2026-05-10 00:19:44 +0000

    Finished ex0101

---
+++ src/ex0101.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int x, y;
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << "Enter y: ";
+    cin >> y;
+
+    cout << "x + y is equal to: " << (x + y) << endl;
+    cout << "x - y is equal to: " << (x - y) << endl;
+    cout << "x * y is equal to: " << (x * y) << endl;
+    cout << " x / y is equal to: " << (x / y) << endl;
+    
+        return 0;
+}
