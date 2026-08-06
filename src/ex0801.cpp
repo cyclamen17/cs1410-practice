5e0339c40d4898a02cfde5bf08f29151c314a437
kaileebarnes_17 <kaileebarnes@mail.weber.edu>
2026-08-06 22:24:14 +0000

    0901 lab practice complete

--- src/ex0801.cpp
+++ src/ex0801.cpp
@@ -5,47 +5,58 @@ using namespace std;
 
 class Author {
 private:
-    string name, nationality;
+    string name;
+    string nationality;
 
-public: 
-Author(const string& names, const string& nationality): name(name), nationality(nationality){}
+public:
+    Author(const string& name, const string& nationality)
+        : name(name), nationality(nationality) {}
 
-string getName() const {return name;}
-string getNationality() const {return nationality;}
+    string getName() const { return name; }
+    string getNationality() const { return nationality; }
 
-void setName(const string& name){this->name = name;}
-void setNationality(const string& nat) {nationality = nat;}
-
+    void setName(const string& name) { this->name = name; }
+    void setNationality(const string& nationality) {
+        this->nationality = nationality;
+    }
 };
 
-class Book{
-private: 
+class Book {
+private:
     string title;
     int year;
     Author author;
 
-public: 
-    Book(const string& t, int y, const Author& a):title(t), year(y), author(a){}
+public:
+    Book(const string& t, int y, const Author& a)
+        : title(t), year(y), author(a) {}
 
-    string getTitle() const {return title;}
-    int getYear() const {return year;}
-    Author getAuthor() const {return author;}
+    string getTitle() const { return title; }
+    int getYear() const { return year; }
+    Author getAuthor() const { return author; }
 
-    void setTitle(const string& a) {title = a;}
-    void setYear(int y){year = y;}
-    void setAuthor(const Author& a) {author = a;}
+    void setTitle(const string& t) { title = t; }
+    void setYear(int y) { year = y; }
+    void setAuthor(const Author& a) { author = a; }
 
-    void show() const{
-        cout << "Book:" << title << "(" << year << ")\n"
-        << "  Author:" << author.getName() << "[" << author.getNationality() << ")\n";
+    void show() const {
+        int currentYear = 2026;
+        int age = currentYear - year;
 
+        cout << "─────────────────" << endl;
+        cout << "Book: " << title << " (" << year << ")" << endl;
+        cout << "Author: " << author.getName()
+             << " [" << author.getNationality() << "]" << endl;
+        cout << "Book Age: " << age << " years" << endl;
+        cout << "─────────────────" << endl;
     }
 };
 
-int main(){
-    Author a ("Emily Dickinson", "American");
+int main() {
+    Author a("Emily Dickinson", "American");
     Book b("The Daisy Follows Soft the Sun", 1865, a);
 
     b.show();
+
     return 0;
 }
